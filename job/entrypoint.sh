09f9863d068ac1ab72393b2af765045644e9f6f6
jiggyjigsj <jigar.patel@pager.com>
2022-10-12 22:31:28 -0500

    adding graylog-init

--- job/entrypoint.sh
+++ job/entrypoint.sh
@@ -13,7 +13,7 @@ function clean {
   ns=$(echo $space | sed 's/%//')
   thres=75
 
-  log "Free space: ${space} and threshold at: ${thres}%"
+  log "Free space: ${space} with threshold at: ${thres}%"
 
   paused=$(curl -m 5 -s http://${USER}:${PASS}@nzbget:6789/jsonrpc/status | jq .result.DownloadPaused)
 
@@ -27,10 +27,79 @@ function clean {
   fi
 }
 
-TIME="${TIME:-60}"
+# function fdclean {
+#   FDPATH=/mnt/storage
+#   IGNORE_HIMYM="tv/How I Met Your Mother"
+#   # find $FDPATH -type f ! -path "$FSPATH/tv/How I Met Your Mother/*" -printf '%T+ %p\n' | sort | head -n 10
+
+#   if [[ -z $FDPATH ]]; then log "Missing env FDPATH"; exit 1; fi
+
+#   space=$(df -h $FDPATH | egrep -o '[0-9]+%')
+#   ns=$(echo $space | sed 's/%//')
+#   thres=85
+
+#   log "Free space: ${space} with threshold at: ${thres}%"
+
+#   if [[ $ns -gt  $thres ]]; then
+#     local ignore_list
+#     log "$FDPATH is lower then $thres"
+#     for var in "${!IGNORE_@}"; do
+#       log "INFO - ${var}=${!var}"
+#       ignore_list="$ignore_list ! -path '$FDPATH/${!var}/*'"
+#     done
+#     log "INFO - IGNORE PATHS $ignore_list"
+
+#     list_of_files=$(find $FDPATH -type f$ignore_list -printf '%T+ %p\n' | sort | head -n 10)
+#     echo list_of_files
+#   fi
+# }
+
+# TIME="${60:-default}"
 
 while :
 do
   clean
   sleep $TIME
 done
+
+#### Below is test case
+#  3001  export   FDPATH=/mnt/storage\n
+#  3013  export IGNORE_HIMYM="tv/How I Met Your Mother"
+#  3020  export IGNORE_HIMYM="tv/How I Met Your Mother"
+#  3026  export IGNORE_HIMYT="tv/How I Met Your Mother"
+#  3064  export IGNORE_HIMYT="tv/1883"
+
+# #!/bin/bash
+# mset -x
+# function log {
+#   echo "[INFO][`date "+%Y/%m/%d %H:%M:%S"`] $1"
+# }
+
+# function fsclean {
+#   FDPATH=/mnt/storage
+#   IGNORE_HIMYM="tv/How I Met Your Mother"
+#   # find $FDPATH -type f ! -path "$FSPATH/tv/How I Met Your Mother/*" -printf '%T+ %p\n' | sort | head -n 10
+
+#   if [[ -z $FDPATH ]]; then log "Missing env FDPATH"; exit 1; fi
+
+#   space=$(df -h $FDPATH | egrep -o '[0-9]+%')
+#   ns=$(echo $space | sed 's/%//')
+#   thres=85
+
+#   log "Free space: ${space} and threshold at: ${thres}%"
+
+#   if [[ $ns -lt  $thres ]]; then
+#     local ignore_list
+#     log "$FDPATH is lower then $thres"
+#     for var in "${!IGNORE_@}"; do
+#       log "INFO - IGNOREING ${var}=${!var}"
+#       ignore_list="$ignore_list ! -path $FDPATH/${!var}/*"
+#     done
+
+#     log "INFO - IGNORE PATHS $ignore_list"
+#     list_of_files=$(find $FDPATH -type f "$ignore_list" -printf '%T+ %p\n' | sort | head -n 10)
+#     echo $list_of_files
+#   fi
+# }
+
+# fsclean
